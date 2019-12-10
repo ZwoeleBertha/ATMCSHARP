a9ed80a59dda6e235e8450cdd51526607c832930
Melissa Cejas <melissacejas@live.nl>
2019-12-10 15:33:37 +0100

    Initial commit

---
+++ ATM.Tests/Test.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace ATM.Tests
+{
+    [TestFixture]
+    public class Test
+    {
+        [Test]
+        public void TestCase()
+        {
+        }
+    }
+}
